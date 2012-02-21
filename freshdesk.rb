fd95d4b59d1fee6a5a138823db750f4d5fbd4396
David Liman <dliman@onescreen.com>
2012-02-20 18:47:54 -0800

    post working

--- freshdesk.rb
+++ freshdesk.rb
@@ -1,6 +1,6 @@
 require "rubygems"
 require 'rest_client'
-require 'pp'
+require 'nokogiri'
 
 =begin
 response = RestClient::Request.new(:method => :get, 
@@ -9,20 +9,29 @@ response = RestClient::Request.new(:method => :get,
                                     :password => '134658').execute
 =end 
 pay = 
-  "<customer><name>freshdesk</name></customer>"
+  "<customer><name>testing</name></customer>"
   
-response = RestClient::Request.new(:method => :post, 
-                                    :url => 'http://onescreen.freshdesk.com/customers.xml', 
-                                    :user => 'limanoit@gmail.com', 
-                                    :password => '134658',
-                                    :payload => pay, 
-                                    :content_type => 'application/xml').execute
-puts response
 
+url = 'http://onescreen.freshdesk.com/customers.xml'
+user = 'limanoit@gmail.com'
+password = '134658'
+
+RestClient.add_before_execution_proc do | req, params |
+  req.basic_auth 'limanoit@gmail.com', '134658'
+end
+
+customer =  Nokogiri::XML("<customer><name>Alf</name></customer>")
+response = RestClient.post url, pay, :content_type => "text/xml"
+
+puts response
 
 class Freshdesk
    
   attr_accessor :url, :username, :password 
+  
+  def initialize(args)
+     
+  end
                                  
   def add_auth(username, password)
     @username, @password = username, password
