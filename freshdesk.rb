1b69aabd0549dad754efc6cd3e3a2c951d319700
David Liman <limanoit@gmail.com>
2012-02-22 00:13:43 -0800

    implemented GET and DELETE

--- freshdesk.rb
+++ freshdesk.rb
@@ -1,17 +1,11 @@
-require "rubygems"
+require 'rubygems'
 require 'rest_client'
 require 'nokogiri'
 
-# Mappings of object name to url:
-#   tickets => helpdesk/tickets.xml
-#   ticket_fields => /ticket_fields.xml
-#   users => /contacts.xml
-#   forums => /categories.xml
-#   solutions => /solution/categories.xml
-#   companies => /customers.xml
+
 class Freshdesk
    
-  attr_accessor :base_url, :username, :password 
+  attr_accessor :base_url
   
   def initialize(base_url, username, password)
     
@@ -22,6 +16,7 @@ class Freshdesk
     end
   end
 
+=begin
   def create_user(args)
     raise ArgumentError if !args.is_a?(Hash)
     
@@ -39,25 +34,86 @@ class Freshdesk
       raise StandardError, "User with the same name already exists"
     end
   end
+=end
   
-  def create_company(args)
-    raise ArgumentError if !args.is_a?(Hash)
-    
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.customer {
-        args.each do |key, value|
-          xml.send(key, value)
-        end
-      }
+  # Freshdesk API client support "GET" with id parameter optional
+  #   Examples: 
+  #     client = Freshdesk.new(url, username, password)
+  #     client.get_tickets     => list all the tickets
+  #     client.get_tickets 123 => view a particular ticket with id 123
+  #
+  #  Will throw: 
+  #   "RestClient::ResourceNotFound if it can not find a particular id or 
+  #                                 if connection can not be established
+  def self.define_get(name, *args)
+    name = name.to_s
+    method_name = "get_" + name
+
+    define_method method_name do |args|
+      uri = mapping(name)
+      uri.gsub!(/.xml/, "/#{args}.xml") if !args.nil?
+      response = RestClient.get uri
+      response
     end
+  end
+  
+  # Freshdesk API client support "DELETE" with the required id parameter
+  #   Examples: 
+  #     client = Freshdesk.new(url, username, password)
+  #     client.delete_tickets 123 => delete a particular ticket with id 123
+  #
+  #  Will throw: 
+  #   "RestClient::ResourceNotFound if it can not find a particular id or 
+  #                                 if connection can not be established
+  def self.define_delete(name, *args)
+    name = name.to_s
+    method_name = "delete_" + name
+
+    define_method method_name do |args|
+      uri = mapping(name)
+      raise StandardError, "An ID is required to delete" if args.nil?
+      uri.gsub!(/.xml/, "/#{args}.xml")
+      RestClient.delete uri
+    end
+  end
+  
+  def self.define_post(name, *args)
+    name = name.to_s
+    method_name = "post_" + name
+    define_method method_name do |args|
+      raise StandardError, "An ID is required to modify data" if args.nil?
+      puts args.class
+      uri = mapping(name)
     
-    begin
-      RestClient.post @base_url + '/customers.xml',  builder.to_xml, :content_type => "text/xml"
-    rescue  RestClient::UnprocessableEntity => e
-      raise StandardError, "Company with the same name already exists"
+    end
+  end
+  
+  [:tickets, :ticket_fields, :users, :forums, :solutions, :companies].each do |a|
+    define_get a
+    define_post a
+    define_delete a
+  end
+    
+  # Mapping of object name to url:
+  #   tickets => helpdesk/tickets.xml
+  #   ticket_fields => /ticket_fields.xml
+  #   users => /contacts.xml
+  #   forums => /categories.xml
+  #   solutions => /solution/categories.xml
+  #   companies => /customers.xml
+  def mapping(method_name)
+    path = case method_name
+      when "tickets" then @base_url + "/helpdesk/tickets.xml"
+      when "ticket_fields" then @base_url + "/ticket_fields.xml"
+      when "users" then @base_url + "/contacts.xml"
+      when "forums" then @base_url + "/categories.xml"
+      when "solutions" then @base_url + "/solution/categories.xml"
+      when "companies" then @base_url + "/customers.xml"
     end
   end
 end
 
 client = Freshdesk.new('http://onescreen.freshdesk.com', 'limanoit@gmail.com', '134658')
-client.create_company({:name => "company2", :email => "my company description" })
+#client.create_company({:name => "cotempany2", :email => "my company description" })
+#client.post_companies( {:name => "cotempany2", :email => "my company description" }, "david")
+client.get_companies("33956")
