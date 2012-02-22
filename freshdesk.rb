5a92d1b7872adb2666ead652008caa2443fe2566
David Liman <limanoit@gmail.com>
2012-02-22 12:17:23 -0800

    need to work on naming convention for POST method

--- freshdesk.rb
+++ freshdesk.rb
@@ -2,9 +2,8 @@ require 'rubygems'
 require 'rest_client'
 require 'nokogiri'
 
-
 class Freshdesk
-   
+  VERSION = "1.0"
   attr_accessor :base_url
   
   def initialize(base_url, username, password)
@@ -15,26 +14,6 @@ class Freshdesk
       req.basic_auth username, password
     end
   end
-
-=begin
-  def create_user(args)
-    raise ArgumentError if !args.is_a?(Hash)
-    
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.user {
-        args.each do |key, value|
-          xml.send(key, value)
-        end
-      }
-    end
-    
-    begin
-      RestClient.post @base_url + '/contacts.xml',  builder.to_xml, :content_type => "text/xml"
-    rescue  RestClient::UnprocessableEntity => e
-      raise StandardError, "User with the same name already exists"
-    end
-  end
-=end
   
   # Freshdesk API client support "GET" with id parameter optional
   #   Examples: 
@@ -42,9 +21,7 @@ class Freshdesk
   #     client.get_tickets     => list all the tickets
   #     client.get_tickets 123 => view a particular ticket with id 123
   #
-  #  Will throw: 
-  #   "RestClient::ResourceNotFound if it can not find a particular id or 
-  #                                 if connection can not be established
+  #   Returns nil if there is no response
   def self.define_get(name, *args)
     name = name.to_s
     method_name = "get_" + name
@@ -52,8 +29,12 @@ class Freshdesk
     define_method method_name do |args|
       uri = mapping(name)
       uri.gsub!(/.xml/, "/#{args}.xml") if !args.nil?
-      response = RestClient.get uri
-      response
+      
+      begin
+        response = RestClient.get uri
+      rescue 
+        response = nil
+      end
     end
   end
   
@@ -77,20 +58,42 @@ class Freshdesk
     end
   end
   
+  # Freshdesk API client support "POST" with the required id parameter
+  #   Examples: 
+  #     client = Freshdesk.new(url, username, password)
+  #     client.post_user :name=> "david", :customer => "google"
+  #       => create a user with the name david and company name google
+  #
+  #  Will throw: 
+  #    RestClient::UnprocessableEntity if the same object already exists or
+  #                                    server does not accept the parameter
   def self.define_post(name, *args)
     name = name.to_s
     method_name = "post_" + name
-    define_method method_name do |args|
-      raise StandardError, "An ID is required to modify data" if args.nil?
-      puts args.class
-      uri = mapping(name)
     
+    define_method method_name do |args|
+      raise StandardError, "Arguments are required to modify data" if args.nil?
+      uri = mapping(name)
+      
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.send(name) {
+          args.each do |key, value|
+            xml.send(key, value)
+          end
+        }
+      end
+      
+      begin 
+        RestClient.post uri, builder.to_xml, :content_type => "text/xml"
+      rescue Error => e
+        raise
+      end
     end
   end
   
   [:tickets, :ticket_fields, :users, :forums, :solutions, :companies].each do |a|
     define_get a
-    define_post a
+    define_post a   # for put operation also
     define_delete a
   end
     
@@ -114,6 +117,6 @@ class Freshdesk
 end
 
 client = Freshdesk.new('http://onescreen.freshdesk.com', 'limanoit@gmail.com', '134658')
-#client.create_company({:name => "cotempany2", :email => "my company description" })
+client.post_users(:name => "cotempany2", :email => "my company description" )
 #client.post_companies( {:name => "cotempany2", :email => "my company description" }, "david")
-client.get_companies("33956")
+#client.get_companies("33956")
